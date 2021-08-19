0af83d38aa87ddc8b40d9599b53f1a850dad6744
Pjossot <joshsamuel19@gmail.com>
2021-08-19 08:14:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+git add app.js
+git commit -m "change message"
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
